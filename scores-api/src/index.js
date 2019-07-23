89555fc9b4f0a71364a2fe293c68c7db2def09a4
Brandon Harris <brandonkharris@gmail.com>
2019-07-23 11:23:54 -0500

    AB{3} Add port logic

--- scores-api/src/index.js
+++ scores-api/src/index.js
@@ -4,6 +4,7 @@ import mlbScoresRouter from "./routes/mlbScores";
 import womensWorldCupRouter from "./routes/womensWorldCup";
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use("/scores/mlb", mlbScoresRouter);
@@ -11,4 +12,4 @@ app.use("/scores/mlb/:id", mlbScoresRouter);
 app.use("/scores/wwc", womensWorldCupRouter);
 app.use("/scores/wwc/:id", womensWorldCupRouter);
 
-app.listen(3000, () => console.log("App listening on port 3000"));
+app.listen(port, () => console.log(`App listening on port ${port}`));
