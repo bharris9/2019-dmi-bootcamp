b9d5cbd81cb31553798287c589ac42c1dd392ee2
Brandon Harris <bharris@dminc.com>
2019-08-05 09:27:05 -0500

    Add ncaa football league endpoint Resolves AB#75

--- scores-api/src/routes/leagues.js
+++ scores-api/src/routes/leagues.js
@@ -1,23 +1,180 @@
-import express from "express";
-import axios from "axios";
-import mapToInternalModel from "../maps/leagues";
+import express from 'express';
+import axios from 'axios';
+import mapToInternalModel from '../maps/leagues';
 
-const baseUri = "http://site.api.espn.com/apis/site/v2/leagues/dropdown?lang=en&region=us&calendartype=whitelist&limit=100&sport=";
+const baseUri =
+  'http://site.api.espn.com/apis/site/v2/leagues/dropdown?lang=en&region=us&calendartype=whitelist&limit=100&sport=';
 const router = express.Router();
 
-router.get("/:sport", async (req, res) => {
+router.get('/:sport', async (req, res) => {
   try {
-    const leagueUri = baseUri + req.params.sport;
-    console.log(leagueUri);
-    const response = await axios.get(leagueUri);
-    const data = response.data;
-    const mapped = mapToInternalModel(data);
+    var sport = req.params.sport;
 
-    res.send(mapped);
+    if (!sport) {
+      res.status(500).json({ error: 'Must supply a sport' });
+    }
+
+    if (sport !== 'college-football') {
+      const leagueUri = baseUri + sport;
+      console.log(leagueUri);
+      const response = await axios.get(leagueUri);
+      const data = response.data;
+      const mapped = mapToInternalModel(data);
+      res.send(mapped);
+    } else {
+      const data = getCollegeFootballLeagues();
+      const mapped = mapToInternalModel(data);
+      res.send(mapped);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.toString() });
   }
 });
+
+function getCollegeFootballLeagues() {
+  return {
+    leagues: [
+      {
+        id: 80,
+        name: 'FBS (1-A)',
+        abbreviation: 'FBS',
+        apiKey: '80',
+        logos: [
+          {
+            href:
+              'https://www.seekpng.com/ipng/u2q8i1u2o0o0t4a9_ncaa-college-football-ncaa-football-logo/'
+          }
+        ]
+      },
+      {
+        id: 1,
+        name: 'Atlantic Coast Conference',
+        abbreviation: 'ACC',
+        apiKey: '1',
+        logos: [{ href: 'http://theacc.com/images/main_logo.png' }]
+      },
+      {
+        id: 151,
+        name: 'American Conference',
+        abbreviation: 'AAC',
+        apiKey: '151',
+        logos: [
+          {
+            href:
+              'https://s3.amazonaws.com/sidearm.sites/theamerican.sidearmsports.com/images/responsive/aac_logo.svg'
+          }
+        ]
+      },
+      {
+        id: 4,
+        name: 'Big 12',
+        abbreviation: 'Big 12',
+        apiKey: '4',
+        logos: [
+          {
+            href:
+              'http://image.cdnllnwnl.xosnetwork.com/fls/10410/site_graphics/logo.png'
+          }
+        ]
+      },
+      {
+        id: 5,
+        name: 'Big 10',
+        abbreviation: 'B1G',
+        apiKey: '5',
+        logos: [
+          { href: 'https://s3.amazonaws.com/bigten.org/images/btc_logo.svg' }
+        ]
+      },
+      {
+        id: 12,
+        name: 'Conference USA',
+        abbreviation: 'CUSA',
+        apiKey: '12',
+        logos: [{ href: 'https://conferenceusa.com/images/logo.png' }]
+      },
+      {
+        id: 18,
+        name: 'FBS Independents',
+        abbreviation: 'FBS Ind',
+        apiKey: '18',
+        logos: [
+          {
+            href:
+              'https://en.wikipedia.org/wiki/NCAA_Division_I_FBS_independent_schools#/media/File:NCAA_Division_I_FBS_independent_schools_logo.png'
+          }
+        ]
+      },
+      {
+        id: 15,
+        name: 'Mid-American Conference',
+        abbreviation: 'MAC',
+        apiKey: '15',
+        logos: [
+          { href: 'https://getsomemaction.com/images/responsive/main_logo.png' }
+        ]
+      },
+      {
+        id: 17,
+        name: 'Mountain West Conference',
+        abbreviation: 'MWC',
+        apiKey: '17',
+        logos: [{ href: 'https://themw.com/images/main_logo.png' }]
+      },
+      {
+        id: 9,
+        name: 'Pac-12 Conference',
+        abbreviation: 'PAC12',
+        apiKey: '9',
+        logos: [
+          {
+            href:
+              'https://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/logo.png'
+          }
+        ]
+      },
+      {
+        id: 8,
+        name: 'Southeastern Conference',
+        abbreviation: 'SEC',
+        apiKey: '8',
+        logos: [
+          {
+            href:
+              'https://a.espncdn.com/secnetwork/prod/images/sec-lockup@2x.png'
+          }
+        ]
+      },
+      {
+        id: 37,
+        name: 'Sun Belt Conference',
+        abbreviation: 'SBC',
+        apiKey: '37',
+        logos: [
+          { href: 'https://sunbeltsports.org/images/responsive/main_logo.png' }
+        ]
+      },
+      {
+        id: 81,
+        name: 'Division 1 FCS',
+        abbreviation: 'FCS',
+        apiKey: '81',
+        logos: [
+          { href: 'https://en.wikipedia.org/wiki/NCAA_Division_I_Football_Championship#/media/File:NCAA_Division_I_FCS_logo.svg' }
+        ]
+      },
+      {
+        id: 35,
+        name: 'Division II/III',
+        abbreviation: 'D2/D3',
+        apiKey: '35',
+        logos: [
+          { href: 'http://www.ncaa.org/sites/all/themes/victory/logo.png' }
+        ]
+      },
+    ]
+  };
+}
 
 export default router;
